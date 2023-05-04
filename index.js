a950947c33034f86427379411223a98ffa3c7332
Anselmo <anselmohenrique191@gmail.com>
2023-05-04 09:21:23 -0300

    Anselmo

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+function greet(name) {
+    return "Ol\u00E1" + name;
+}
+console.log(greet('Anslemo Henrique'));
---
+++ index.ts
@@ -0,0 +1,5 @@
+function greet(name: string){
+    return "Ol\u00E1" + name;
+}
+
+console.log(greet('Anslemo Henrique'));
